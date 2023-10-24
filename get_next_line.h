da92c211f20032bf0a26c765c60570d177fbdf0c
Seán Belometti <sean.belometti@gmail.com>
2023-10-24 16:02:56 +0200

    cest que le debut du travail ya rien d'iinteressant
    tkt

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbelomet <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 14:08:19 by sbelomet          #+#    #+#             */
+/*   Updated: 2023/10/24 15:10:50 by sbelomet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
